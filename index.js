6d0b7257fc4f2db147de692454439a68f2734495
JRCcoding <jrccoding@gmail.com>
2022-08-14 00:27:09 -0500

    full page temporarily to view php information

---
+++ index.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//     var x = document.getElementById("navitem");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   } 
